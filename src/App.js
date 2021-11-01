bd23bf3b00cac350d047590cb14956a4fb36617a
SlntCoder <ask.sunn@live.com>
2021-11-01 14:51:00 +0100

    Added the sign in and form input components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header';
+import Authenticate from './pages/authenticate/Authenticate';
 
 
 
@@ -11,10 +13,11 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-
+        <Header />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/shop' component={Shop} />
+          <Route exact path='/auth' component={Authenticate} />
         </Switch>
       </div>
     );
---
+++ src/pages/authenticate/Authenticate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './authenticate.scss';
+
+import SignIn from '../../components/signin/SignIn';
+
+const Authenticate = () => (
+  <div className='authenticate'>
+
+    <SignIn />
+
+  </div>
+)
+
+export default Authenticate
